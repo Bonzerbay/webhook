7562ef3d86a54acb5e737223b3147f40afd734ce
Bonzerbay <bonzerbay@gmail.com>
2023-03-28 20:56:16 +0100

    v3 logip

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.post('/webhook', (req, res)=>{
     console.log('payment successful!');
     res.sendStatus(200)
     console.log(req.body);
+    console.log(req.ip);
 })
 
 app.listen(3000, ()=> console.log('Connected'));
