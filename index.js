bf73c7c153e914f6f188b9b5ec8d8b91191b8fe0
Kodrillar <ipadeoladavid112@gmail.com>
2023-05-14 18:56:54 +0100

    access metadata

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.post('/webhook', (req, res)=>{
     console.log('payment successful!');
     res.sendStatus(200)
     console.log(req.body);
+    console.log(req.body.data.metadata.custom_fields[0]);
     console.log(req.ip);
 })
 
